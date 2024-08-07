4e5b0ae16216170a696cf55cfd393dc67b0bacbd
silenakhi <silenakhil@gmail.com>
2024-08-07 20:16:17 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,9 @@ import { signInWithPopup,GoogleAuthProvider,FacebookAuthProvider } from "firebas
 import Survey from "./survey/page"
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
-
-
-
 const login = ()=>{
     const [user, setUser] = useState(null)
     const router = useRouter()
-
     useEffect(()=>{
         const auth = getAuth(app)
       const unsubscribe = auth.onAuthStateChanged((user)=>{
@@ -22,23 +18,19 @@ const login = ()=>{
             }else{
                 setUser(null)
             }
-
         })
         return () => unsubscribe()
     },[])
-
     const signInWithGoogle = async ()=>{
         const provider = new GoogleAuthProvider()
         const auth = getAuth(app)
         try{
             await signInWithPopup(auth,provider)
             router.push("/survey")
-
         }catch(error){
             console.error("Error signing in with Google",error.message)
         }
     }
-
     const signInWithFacebook = async ()=>{
         const provider = new FacebookAuthProvider()
         
@@ -46,21 +38,13 @@ const login = ()=>{
         try{
             await signInWithPopup(auth,provider)
             router.push("/survey")
-
         }catch(error){
             console.error("Error signing in with Facebook",error.message)
         }
     }
 
-    const signIn = async ()=>{
-        try{
-            router.push("/survey")
-                    }
-        catch(error){
-            console.error("Error signing in with Facebook",error.message)
-        }    
-    }
-    
+    const signIn = router.push("/survey")
+
     return (
       <div className="flex flex-col md:flex-row h-screen">
         <div className="flex flex-col justify-center items-center w-full bg-gray-100 p-6 md:p-12 shadow-lg">
@@ -74,7 +58,7 @@ const login = ()=>{
               <div className="flex flex-col gap-4 w-full">
                 <button
                   onClick={signInWithGoogle}
-                  className="flex gap-3 justify-center items-center bg-blue-400 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 py-3 px-5 rounded-full transition duration-150 ease-in-out"
+                  className="flex gap-3 justify-center items-center bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 py-3 px-5 rounded-full transition duration-150 ease-in-out"
                 >
                   <FcGoogle className="text-2xl" />
                   <span className="text-lg font-medium">
@@ -82,7 +66,7 @@ const login = ()=>{
                   </span>
                 </button>
                 <button
-                  //onClick={signInWithFacebook}
+                  onClick={signInWithFacebook}
                   className="flex gap-3 justify-center items-center bg-blue-700 text-white hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50 py-3 px-5 rounded-full transition duration-150 ease-in-out"
                 >
                   <FaFacebook className="text-2xl" />
@@ -90,14 +74,14 @@ const login = ()=>{
                     Sign in with Facebook
                   </span>
                 </button>
-               // <button
-                  //onClick={signIn}
-               //   className="flex gap-3 justify-center items-center bg-blue-900 text-white hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50 py-3 px-5 rounded-full transition duration-150 ease-in-out"
-               // >
-               //   <span className="text-lg font-medium">
-               //    Sign In
-               //   </span>
-               // </button>
+                <button
+                  onClick={signIn}
+                  className="flex gap-3 justify-center items-center bg-grey-700 text-white hover:bg-black-800 focus:outline-none focus:ring-2 focus:ring-black-700 focus:ring-opacity-50 py-3 px-5 rounded-full transition duration-150 ease-in-out"
+                >
+                  <span className="text-lg font-medium">
+                    Sign In
+                  </span>
+                </button>
               </div>
             </div>
           )}
